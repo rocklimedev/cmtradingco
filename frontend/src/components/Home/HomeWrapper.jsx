cd058a51bdd4fe349def5af47190cd6b9b17ec15
Dhruvermafz <vermadhruv09112002@gmail.com>
2025-06-12 14:37:53 +0530

    added and fixed it

--- frontend/src/components/Home/HomeWrapper.jsx
+++ frontend/src/components/Home/HomeWrapper.jsx
@@ -53,18 +53,26 @@ const HomeWrapper = () => {
   return (
     <>
       <div className="main-slider">
-        <img src={slider} alt="Slider Background" />
-        <div className="slider-info">
-          <a href="#">Chhabra Marble</a>
-          <p>
-            Your trusted one-stop shop for premium marble, granite, tiles, kota
-            stone, and sanitary ware. With 30+ years of experience, we offer
-            top-quality materials, competitive prices, and timely delivery –
-            built on lasting relationships and trust.
-          </p>
-          <button>Connect With Us</button>
+        <img
+          src={slider}
+          alt="Slider Background"
+          className="slider-background"
+        />
+
+        <div className="slider-overlay">
+          <div className="slider-content">
+            <h1>Chhabra Marble</h1>
+            <p>
+              Your trusted one-stop shop for premium marble, granite, tiles,
+              kota stone, and sanitary ware. With 30+ years of experience, we
+              offer top-quality materials, competitive prices, and timely
+              delivery – built on lasting relationships and trust.
+            </p>
+            <button>Connect With Us</button>
+          </div>
         </div>
       </div>
+
       <section className="home-about-section">
         <h2 className="home-about-span">About</h2>
         <p className="home-about-p">
